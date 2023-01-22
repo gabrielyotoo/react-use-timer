835e06214abf38d472f56fd53c87470c1357e5e3
Gabriel Freitas Yamamoto <gabrielyamamoto82@gmail.com>
2023-01-21 22:09:58 -0300

    feat(example): add hook

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import useTimer from './useTimer';
 
 function App() {
+  const [finished, setFinished] = useState(false);
+
+  const { currentTime, running, startTimer, pauseTimer } = useTimer(10, {
+    runEvery: 500,
+    onFinish: () => {
+      setFinished(true);
+    },
+    onStart: () => {
+      setFinished(false);
+    },
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {finished ? <p>Finished!</p> : null}
+        <div style={{ flexDirection: 'row' }}>
+          {!running ? (
+            <p>Not running! Timer at {currentTime}</p>
+          ) : (
+            <p>Timer at {currentTime}</p>
+          )}
+          <button onClick={startTimer}>{!running ? 'Start!' : 'Reset'}</button>
+          {running ? <button onClick={pauseTimer}>Pause</button> : null}
+        </div>
       </header>
     </div>
   );
