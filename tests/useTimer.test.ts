b22f9c55694ef915bd0a541787b69908dee2bb6c
Gabriel Freitas Yamamoto <gabrielyamamoto82@gmail.com>
2023-05-18 20:18:57 -0300

    feat(tests): add more tests

--- tests/useTimer.test.ts
+++ tests/useTimer.test.ts
@@ -1,4 +1,4 @@
-import { renderHook, act } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react-hooks';
 import { expect, test } from 'vitest';
 import useTimer from '../src/useTimer';
 
@@ -19,19 +19,57 @@ test('should start the timer automatically if autoStart is set', async () => {
 });
 
 test('should call onStart function if it exists', () => {
-  // to be done
+  let called = false;
+  const { result } = renderHook(() =>
+    useTimer(5, {
+      onStart: () => {
+        called = true;
+      },
+    })
+  );
+
+  result.current.startTimer();
+
+  expect(called).toBeTruthy();
 });
 
-test('should decrease current time at time specified', () => {
-  // to be done
+test('should decrease current time at time specified', async () => {
+  const { result } = renderHook(() => useTimer(5, { runEvery: 200 }));
+
+  result.current.startTimer();
+
+  expect(result.current.currentTime).toBe(5);
+  await sleep(100);
+  expect(result.current.currentTime).toBe(5);
+  await sleep(100);
+  expect(result.current.currentTime).toBe(4);
 });
 
-test('should pause the timer when pause is requested', () => {
-  // to be done
+test('should pause the timer when pause is requested', async () => {
+  const { result } = renderHook(() => useTimer(5, { runEvery: 100 }));
+
+  result.current.startTimer();
+
+  expect(result.current.currentTime).toBe(5);
+  await sleep(200);
+  expect(result.current.currentTime).toBe(4);
+
+  result.current.pauseTimer();
+  await sleep(200);
+  expect(result.current.currentTime).toBe(4);
 });
 
-test('should stop the timer when times up', () => {
-  // to be done
+test('should stop the timer when times up', async () => {
+  const { result } = renderHook(() => useTimer(2, { runEvery: 100 }));
+
+  result.current.startTimer();
+
+  expect(result.current.currentTime).toBe(2);
+  await sleep(200);
+
+  expect(result.current.currentTime).toBe(0);
+  await sleep(100);
+  expect(result.current.currentTime).toBe(0);
 });
 
 test('should call onFinish function if it exists', () => {
--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    include: ['./tests/**/**.test.**'],
+    include: ['./tests/**.test.**'],
     environment: 'jsdom',
     setupFiles: './tests/setup.ts',
   },
